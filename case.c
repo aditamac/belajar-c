dc9e87e2467c0626ccb80ec652402697602214c5
krisnasona <sumberterbuka16@gmail.com>
2019-05-17 13:09:13 +0700

    menambahkan file case.c

---
+++ case.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(void)
+/* - Pengertian sederhana dari case sensitifity adalah perbedaan antara huruf besar dan huruf kecil. 
+   - Bahasa C termasuk bahasa yang case sensitif
+*/
+{
+  Printf("Hello, World!\n");    /* error !!. Perintah printf tidak bisa ditulis menjadi Printf */
+  return 0;
+}
