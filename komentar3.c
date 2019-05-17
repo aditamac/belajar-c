20c0c7febc7b78897e35dea6e4f8819f1f0b8dda
krisnasona <sumberterbuka16@gmail.com>
2019-05-17 13:45:24 +0700

    menambah file komentar3.c ke folder 2

---
+++ komentar3.c
@@ -0,0 +1,12 @@
+// Pembuat  : Duniailkom.com
+// Tujuan   : Menampilkan pesan "Hello, World!"
+// Bahasa   : C
+// Tanggal Pembuatan: 31 Mei 2017
+// komentar dengan dua slash "//"  
+#include <stdio.h>
+int main(void)
+{
+  // Tampilkan pesan Hello World
+  printf("Hello, World!\n");
+  return 0;
+}
