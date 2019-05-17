55be21b11295df51ace23ed5e41c548b2a43ddee
krisnasona <sumberterbuka16@gmail.com>
2019-05-17 14:08:07 +0700

    menambah file statement.c di folder 2

---
+++ 2_aturan_dasar_penulisan_c/statement.c
@@ -0,0 +1,21 @@
+/*
+- Statement dalam bahasa C adalah sebuah baris perintah
+- Setiap baris perintah (statement) dalam bahasa C harus diakhiri dengan tanda titik koma ( ; )
+*/
+x = 7; // Ini satu baris statement
+y = "krisnasona"; // Ini satu baris statement
+z = 3+1; // Ini satu baris statement
+1+5; // Ini satu baris statement
+puts("krisnasona"); // Ini satu baris statement
+root2 = sqrt(59); // Ini satu baris statement
+/*
+- Sebuah statement tidak harus mencakup satu baris
+- Perintah kondisi seperti if else dan perulangan (looping) bisa mencakup beberapa baris, dan ini dianggap sebagai satu statement
+*/
+if( x == "krisnasona") {
+ printf("Belajar Bahasa C");
+}
+/*
+- Statement yang terdiri dari beberapa baris seperti ini, biasanya diawali dengan tanda kurung kurawal ” { ” dan diakhiri dengan tanda ” } “
+- Bagian ini juga sering disebut sebagai blok kode program
+*/
