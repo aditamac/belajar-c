ac20b2879f2305f539d8a6baa399fde9d14e423b
krisnasona <sumberterbuka16@gmail.com>
2019-05-17 15:46:56 +0700

    menambah file keyword.c di folder 2

---
+++ 2_aturan_dasar_penulisan_c/keyword.c
@@ -0,0 +1,19 @@
+/*
+- Keyword adalah kata kunci yang menjadi dasar perintah bahasa C
+- Keyword ini tidak bisa digunakan sebagai identifier (variabel, konstanta maupun nama dari sebuah fungsi)
+*/
+int jumlah;
+return 0;
+/*
+- Disini, kata int dan return merupakan keyword dan memiliki makna tertentu di dalam bahasa C
+- Terdapat beberapa kata kunci (keyword) dalam bahasa pemrograman C, yakni:
+auto	double	int	struct
+break	else	long	switch
+case	enum	register	typedef
+char	extern	return	union
+continue	for	signed	void
+do	if	static	while
+default	goto	sizeof	volatile
+const	float	short	unsigned
+- Seluruh kata ini tidak bisa digunakan sebagai nama variabel / konstanta dalam bahasa C
+*/
