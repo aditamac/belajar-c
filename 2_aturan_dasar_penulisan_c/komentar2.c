dedd0687371006ad9739036960cdf53bd2792e1e
krisnasona <sumberterbuka16@gmail.com>
2019-05-17 13:43:00 +0700

    menambah file komentar2.c ke folder 2

---
+++ 2_aturan_dasar_penulisan_c/komentar2.c
@@ -0,0 +1,13 @@
+// digunakan untuk membuat semacam “copyright”
+/* Pembuat  : krisnasona
+ * Tujuan   : Menampilkan pesan "Hello, World!"
+ * Bahasa   : C
+ * Tanggal Pembuatan: 17 Mei 2019
+ */
+  
+#include <stdio.h>
+int main(void)
+{
+  printf("Hello, World!\n");
+  return 0;
+}
