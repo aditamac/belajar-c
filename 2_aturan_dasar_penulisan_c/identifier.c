377238a1fa414bb2173a6057a0302d340d61b0ad
krisnasona <sumberterbuka16@gmail.com>
2019-05-17 16:05:59 +0700

    menambah file identifier.c ke folder 2

---
+++ 2_aturan_dasar_penulisan_c/identifier.c
@@ -0,0 +1,5 @@
+/* - Identifier adalah nama untuk menandakan “sesuatu” sepanjang kode program
+   - Contoh dari identifier adalah variabel, konstanta dan fungsi
+*/
+int jumlah; // jumlah = identifier
+double total_penjualan; // total_penjualan = identifier
