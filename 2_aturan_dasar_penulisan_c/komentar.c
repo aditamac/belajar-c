e6d10485d4b6c8e82ec25c30859a49185a3768e0
krisnasona <sumberterbuka16@gmail.com>
2019-05-17 13:36:51 +0700

    menambah file komentar.c ke folder 2

---
+++ 2_aturan_dasar_penulisan_c/komentar.c
@@ -0,0 +1,8 @@
+// Komentar atau comment adalah ‘kode program’ yang ditambahkan untuk memberi keterangan/penjelasan mengenai cara kerja program
+#include <stdio.h>
+int main(void)
+{
+    /* Komentar untuk tampilkan pesan Hello World */
+    printf("Hello, World!\n");
+    return 0;
+}
